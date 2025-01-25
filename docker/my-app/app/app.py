6b6fbfbb2e4cbb12ece3bad83e29c3ca59b70e26
lepc1972 <githublepc@gmail.com>
2025-01-24 23:44:13 -0500

    app.py

--- docker/my-app/app/app.py
+++ docker/my-app/app/app.py
@@ -3,10 +3,12 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     db_url = os.getenv("DATABASE_URL", "No database URL set")
     return f"Hello, Jikkosoft! Database URL is: {db_url}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
