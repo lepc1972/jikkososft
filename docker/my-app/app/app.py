3669d832e5091785c8a5bbc760fdbc28c2b9d7b7
lepc1972 <githublepc@gmail.com>
2025-01-24 21:14:40 -0500

    venv

---
+++ docker/my-app/app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    db_url = os.getenv("DATABASE_URL", "No database URL set")
+    return f"Hello, Docker! Database URL is: {db_url}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
